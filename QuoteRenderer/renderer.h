d19aadd3dede5efc831461b39228ae62d35d81fd
inquaterni <m.matzkevich@gmail.com>
2025-07-10 14:44:33 +0300

    Revert "resctucturization"
    This reverts commit 37fde9f86c5644494cf2bdd307ea8d7ddd8bbdfc.
    
    fixing project structure

---
+++ QuoteRenderer/renderer.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace quote
+{
+    class renderer;
+}
+
+
+namespace quote
+{
+#ifdef _DEBUG
+
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+    class renderer
+    {
+
+        std::vector<sf::Drawable*> drawables_;
+        
+    public:
+        explicit renderer() noexcept = default;
+
+        void add_drawable(sf::Drawable* shape);
+        void reserve(const unsigned int& count);
+
+        void render(const unsigned short& width, const unsigned short& height);
+    };
+
+#else
+
+    class renderer
+    {
+    public:
+        
+    };
+    
+#endif
+}
