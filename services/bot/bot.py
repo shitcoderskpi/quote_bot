7506b7c4bfcfae6d839c9254c65c733f166a0bad
inquaterni <m.matzkevich@gmail.com>
2025-09-05 14:53:29 +0300

    Revert "trying to fix bad base64 in bot.py"
    This reverts commit 036c862414e194e6d1f1a844be8bb79c2040eff2.

--- services/bot/bot.py
+++ services/bot/bot.py
@@ -29,7 +29,6 @@ redis = RedisQueue(REDIS_HOST)
 
 REQUEST_TIME = Summary("request_processing_time", "Time spent processing requests")
 
-
 @dataclass
 class SerializableMessage:
     username: str
@@ -39,36 +38,29 @@ class SerializableMessage:
     chat_id: int
 
     def to_json(self):
-        return dumps(
-            {
-                "chat-id": self.chat_id,
-                "username": self.username,
-                "user_status": self.user_status,
-                "content": self.content,
-                "image": b64encode(self.image.read()).decode("utf-8"),
-            },
-            ensure_ascii=False,
-        ).encode("utf-8")
+        return dumps({
+            "chat-id": self.chat_id,
+            "username": self.username,
+            "user_status": self.user_status,
+            "content": self.content,
+            "image": b64encode(self.image.read()).decode("utf-8")
+            }, ensure_ascii=False).encode("utf-8")
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
-
 async def get_member(msg: Message):
     logger.debug("Getting chat member ...")
     return await bot.get_chat_member(msg.chat.id, msg.from_user.id)
-
 
 async def get_photos(msg: Message, limit: int = 1):
     logger.debug(f"Getting {msg.from_user.full_name}'s photos ...")
     return await bot.get_user_profile_photos(msg.from_user.id, limit=limit)
 
-
 def get_member_custom_title(member):
     return getattr(member, "custom_title", None)
-
 
 @REQUEST_TIME.time()
 @dp.message(Command(compile("q(oute)?")), F.chat.type.in_({"group", "supergroup"}))
@@ -85,22 +77,11 @@ async def command_quote_handler(message: Message) -> None:
     photos = await get_photos(reply)
     if photos.total_count:
         logger.debug("Downloading image ...")
-        file = await bot.get_file(photos.photos[0][0].file_id)
-        photo_bytes = await bot.download(file)
-
-        avatar.write(photo_bytes.read())
-        avatar.seek(0)
-
+        await bot.download(photos.photos[0][0], avatar)
     else:
         logger.warning(f"User {reply.from_user.id} does not have any photos.")
 
-    msg = SerializableMessage(
-        reply.from_user.full_name,
-        get_member_custom_title(member),
-        reply.text,
-        avatar,
-        reply.chat.id,
-    )
+    msg = SerializableMessage(reply.from_user.full_name, get_member_custom_title(member), reply.text, avatar, reply.chat.id)
     await redis.enqueue("generate:jobs", msg.to_json())
     img = await redis.dequeue("generate:results")
 
