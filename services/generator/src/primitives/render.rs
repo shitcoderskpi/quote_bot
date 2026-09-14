eeffe898e9b43c05ceecf6ec770123195d75f0d9
inquaterni <m.matzkevich@gmail.com>
2026-09-14 16:04:20 +0300

    cleanup after merge

--- services/generator/src/primitives/render.rs
+++ services/generator/src/primitives/render.rs
@@ -7,7 +7,6 @@ use rand::rngs::SmallRng;
 use rand::{RngExt, SeedableRng};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-use std::iter::Map;
 use std::ops::Range;
 use taffy::TaffyError;
 use taffy::prelude::*;
