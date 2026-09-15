14bd6a4b35228695958271e66b34d94814786643
dehobitto <dadgafk@gmail.com>
2026-09-16 01:38:06 +0300

    refactor(bot): split lifecycle helpers

--- services/bot/internal/bot/app.go
+++ services/bot/internal/bot/app.go
@@ -2,47 +2,11 @@ package bot
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"syscall"
 	"time"
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
-
-func setupLogging(logPath string) *os.File {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	if logPath == "" {
-		return nil
-	}
-
-	if err := os.MkdirAll(logPath, 0755); err != nil {
-		log.Fatalf("error creating log directory: %v", err)
-	}
-
-	f, err := os.OpenFile(filepath.Join(logPath, "bot-service.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening log file: %v", err)
-	}
-
-	log.SetOutput(f)
-
-	return f
-}
-
-func waitForShutdown(updater *ext.Updater) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	log.Println("Stopping bot...")
-	if err := updater.Stop(); err != nil {
-		log.Printf("failed to stop updater: %v", err)
-	}
-	log.Println("Bot stopped")
-}
 
 func Run() {
 	config := LoadConfig()
