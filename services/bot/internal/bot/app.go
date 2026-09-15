fa8cef4c0f2ad78f85079314c8b9d5f1cf63eb04
dehobitto <dadgafk@gmail.com>
2026-09-16 01:37:38 +0300

    refactor(bot): move dispatcher setup

--- services/bot/internal/bot/app.go
+++ services/bot/internal/bot/app.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
 func setupLogging(logPath string) *os.File {
@@ -31,23 +30,6 @@ func setupLogging(logPath string) *os.File {
 	log.SetOutput(f)
 
 	return f
-}
-
-func newDispatcher(queue Queue) *ext.Dispatcher {
-	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
-		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
-			log.Printf("an error occurred while handling update: %v", err)
-			return ext.DispatcherActionNoop
-		},
-		MaxRoutines: ext.DefaultMaxRoutines,
-	})
-
-	quote := quoteHandler(queue)
-	dispatcher.AddHandler(handlers.NewCommand("start", startHandler))
-	dispatcher.AddHandler(handlers.NewCommand("q", quote))
-	dispatcher.AddHandler(handlers.NewCommand("quote", quote))
-
-	return dispatcher
 }
 
 func waitForShutdown(updater *ext.Updater) {
