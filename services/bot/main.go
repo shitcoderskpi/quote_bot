bc60f196c15100e3e3d2dfe9da348841bf31a119
dehobitto <dadgafk@gmail.com>
2026-09-16 01:32:05 +0300

    refactor(bot): split main setup

--- services/bot/main.go
+++ services/bot/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
 	"time"
 
@@ -13,34 +14,73 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
+func setupLogging(logPath string) *os.File {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	if logPath == "" {
+		return nil
+	}
+
+	if err := os.MkdirAll(logPath, 0755); err != nil {
+		log.Fatalf("error creating log directory: %v", err)
+	}
+
+	f, err := os.OpenFile(filepath.Join(logPath, "bot-service.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening log file: %v", err)
+	}
+
+	log.SetOutput(f)
+
+	return f
+}
+
+func newDispatcher(redisQueue *RedisQueue) *ext.Dispatcher {
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+			log.Printf("an error occurred while handling update: %v", err)
+			return ext.DispatcherActionNoop
+		},
+		MaxRoutines: ext.DefaultMaxRoutines,
+	})
+
+	quote := quoteHandler(redisQueue)
+	dispatcher.AddHandler(handlers.NewCommand("start", startHandler))
+	dispatcher.AddHandler(handlers.NewCommand("q", quote))
+	dispatcher.AddHandler(handlers.NewCommand("quote", quote))
+
+	return dispatcher
+}
+
+func waitForShutdown(updater *ext.Updater) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	log.Println("Stopping bot...")
+	if err := updater.Stop(); err != nil {
+		log.Printf("failed to stop updater: %v", err)
+	}
+	log.Println("Bot stopped")
+}
+
 func main() {
 	config := LoadConfig()
 
-	if config.LogPath != "" {
-		if err := os.MkdirAll(config.LogPath, 0755); err != nil {
-			log.Fatalf("error creating log directory: %v", err)
-		}
-		f, err := os.OpenFile(config.LogPath+"bot-service.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("error opening log file: %v", err)
-		}
-		defer func(f *os.File) {
-			err := f.Close()
-			if err != nil {
-				log.Fatalf("Failed to close log file: %v", err)
+	logFile := setupLogging(config.LogPath)
+	if logFile != nil {
+		defer func() {
+			if err := logFile.Close(); err != nil {
+				log.Printf("Failed to close log file: %v", err)
 			}
-		}(f)
-		log.SetOutput(f)
+		}()
 	}
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
 	redisQueue := NewRedisQueue(config.RedisHost)
-	defer func(redisQueue *RedisQueue) {
-		err := redisQueue.Close()
-		if err != nil {
+	defer func() {
+		if err := redisQueue.Close(); err != nil {
 			log.Printf("Failed to close Redis queue: %v", err)
 		}
-	}(redisQueue)
+	}()
 
 	ctx := context.Background()
 	_ = redisQueue.Delete(ctx, "generate:jobs")
@@ -51,21 +91,8 @@ func main() {
 		log.Fatalf("failed to create bot: %v", err)
 	}
 
-	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
-		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
-			log.Printf("an error occurred while handling update: %v", err)
-			return ext.DispatcherActionNoop
-		},
-		MaxRoutines: ext.DefaultMaxRoutines,
-	})
-
-	updater := ext.NewUpdater(dispatcher, nil)
-
-	dispatcher.AddHandler(handlers.NewCommand("start", startHandler))
-	dispatcher.AddHandler(handlers.NewCommand("q", quoteHandler(redisQueue)))
-	dispatcher.AddHandler(handlers.NewCommand("quote", quoteHandler(redisQueue)))
-
-	err = updater.StartPolling(b, &ext.PollingOpts{
+	updater := ext.NewUpdater(newDispatcher(redisQueue), nil)
+	if err := updater.StartPolling(b, &ext.PollingOpts{
 		DropPendingUpdates: true,
 		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
 			Timeout: 9,
@@ -73,20 +100,10 @@ func main() {
 				Timeout: time.Second * 10,
 			},
 		},
-	})
-	if err != nil {
+	}); err != nil {
 		log.Fatalf("failed to start polling: %v", err)
 	}
+
 	log.Printf("%s has been started...\n", b.User.Username)
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	log.Println("Stopping bot...")
-	err = updater.Stop()
-	if err != nil {
-		log.Printf("failed to stop updater: %v", err)
-	}
-	log.Println("Bot stopped")
+	waitForShutdown(updater)
 }
