e62f5f09f2925561f03d00e439b121f03768af99
dehobitto <dadgafk@gmail.com>
2026-09-16 10:24:03 +0300

    fix(bot): harden quote generation flow

--- services/bot/internal/handlers.go
+++ services/bot/internal/handlers.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"html"
 	"io"
@@ -20,9 +21,62 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+var avatarClient = &http.Client{Timeout: 10 * time.Second}
+
 func startHandler(b *gotgbot.Bot, ctx *ext.Context) error {
 	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, <b>%s</b>!", html.EscapeString(ctx.EffectiveUser.FirstName)), &gotgbot.SendMessageOpts{ParseMode: "HTML"})
 	return err
+}
+
+func optionalString(s string) *string {
+	if s == "" {
+		return nil
+	}
+	return proto.String(s)
+}
+
+func optionalInt32(v *int) *int32 {
+	if v == nil {
+		return nil
+	}
+	return proto.Int32(int32(*v))
+}
+
+func userAvatar(b *gotgbot.Bot, userID int64) []byte {
+	photos, err := b.GetUserProfilePhotos(userID, &gotgbot.GetUserProfilePhotosOpts{Limit: 1})
+	if err != nil {
+		log.Printf("Failed to get profile photos for user %d: %v", userID, err)
+		return nil
+	}
+	if photos.TotalCount == 0 {
+		log.Printf("User %d does not have any photos", userID)
+		return nil
+	}
+
+	file, err := b.GetFile(photos.Photos[0][0].FileId, nil)
+	if err != nil {
+		log.Printf("Failed to get profile photo file for user %d: %v", userID, err)
+		return nil
+	}
+
+	resp, err := avatarClient.Get(file.URL(b, nil))
+	if err != nil {
+		log.Printf("Failed to download profile photo for user %d: %v", userID, err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Failed to download profile photo for user %d: status %d", userID, resp.StatusCode)
+		return nil
+	}
+
+	var avatar bytes.Buffer
+	if _, err := io.Copy(&avatar, io.LimitReader(resp.Body, 5<<20)); err != nil {
+		log.Printf("Failed to read profile photo for user %d: %v", userID, err)
+		return nil
+	}
+	return avatar.Bytes()
 }
 
 func quoteHandler(queue Queue) func(b *gotgbot.Bot, ctx *ext.Context) error {
@@ -54,6 +108,10 @@ func quoteHandler(queue Queue) func(b *gotgbot.Bot, ctx *ext.Context) error {
 			_, _ = msg.Reply(b, "Please specify a message for quote generation", nil)
 			return nil
 		}
+		if reply.From == nil {
+			_, _ = msg.Reply(b, "Can't quote messages without an author", nil)
+			return nil
+		}
 
 		chatMember, err := b.GetChatMember(reply.Chat.Id, reply.From.Id, nil)
 		if err != nil {
@@ -75,24 +133,7 @@ func quoteHandler(queue Queue) func(b *gotgbot.Bot, ctx *ext.Context) error {
 			userStatus = "member"
 		}
 
-		var avatar bytes.Buffer
-		photos, err := b.GetUserProfilePhotos(reply.From.Id, &gotgbot.GetUserProfilePhotosOpts{Limit: 1})
-		if err == nil && photos.TotalCount > 0 {
-			photo := photos.Photos[0][0]
-			file, err := b.GetFile(photo.FileId, nil)
-			if err == nil {
-				resp, err := http.Get(file.URL(b, nil))
-				if err == nil {
-					defer resp.Body.Close()
-					_, err := io.Copy(&avatar, resp.Body)
-					if err != nil {
-						return err
-					}
-				}
-			}
-		} else {
-			log.Printf("User %d does not have any photos or failed to get them", reply.From.Id)
-		}
+		avatar := userAvatar(b, reply.From.Id)
 
 		var text string
 		var entities []gotgbot.MessageEntity
@@ -106,28 +147,17 @@ func quoteHandler(queue Queue) func(b *gotgbot.Bot, ctx *ext.Context) error {
 
 		convertedEntities := convertEntities(text, entities)
 
-		var customTitlePtr *string
-		if customTitle != "" {
-			customTitlePtr = &customTitle
-		}
-
-		var dpiPtr *int32
-		if dpi != nil {
-			val := int32(*dpi)
-			dpiPtr = &val
-		}
-
 		serializableMsg := &pb.SerializableMessage{
 			GradId:     int32(reply.From.Id % 7),
 			Username:   reply.From.FirstName,
-			UserStatus: customTitlePtr,
-			UserRole:   &userStatus,
+			UserStatus: optionalString(customTitle),
+			UserRole:   proto.String(userStatus),
 			Content:    text,
 			Entities:   convertedEntities,
-			Image:      avatar.Bytes(),
+			Image:      avatar,
 			MessageId:  reply.MessageId,
 			ChatId:     reply.Chat.Id,
-			Dpi:        dpiPtr,
+			Dpi:        optionalInt32(dpi),
 			Theme:      theme,
 		}
 
@@ -137,25 +167,39 @@ func quoteHandler(queue Queue) func(b *gotgbot.Bot, ctx *ext.Context) error {
 			return err
 		}
 
-		encoder, _ := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
+		encoder, err := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
+		if err != nil {
+			log.Printf("Failed to create zstd encoder: %v", err)
+			return err
+		}
 		compressedData := encoder.EncodeAll(pbData, make([]byte, 0, len(pbData)))
 
 		startMs := time.Now().UnixMilli()
-		ctxBg := context.Background()
+		jobCtx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
 
-		err = queue.Enqueue(ctxBg, "generate:jobs", compressedData)
+		err = queue.Enqueue(jobCtx, "generate:jobs", compressedData)
 		if err != nil {
 			log.Printf("Failed to enqueue job: %v", err)
 			return err
 		}
 
-		resultData, err := queue.Dequeue(ctxBg, "generate:results", 0)
+		resultData, err := queue.Dequeue(jobCtx, "generate:results", 0)
 		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) || errors.Is(err, context.Canceled) {
+				_, _ = msg.Reply(b, "Quote generation timed out", nil)
+				return nil
+			}
 			log.Printf("Failed to dequeue result: %v", err)
 			return err
 		}
 
-		decoder, _ := zstd.NewReader(nil)
+		decoder, err := zstd.NewReader(nil)
+		if err != nil {
+			log.Printf("Failed to create zstd decoder: %v", err)
+			return err
+		}
+		defer decoder.Close()
 		decompressedData, err := decoder.DecodeAll(resultData, nil)
 		if err != nil {
 			log.Printf("Failed to decompress result: %v", err)
